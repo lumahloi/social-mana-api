b480ba617e39b9cb06f9b6f1feb968ff2a94cdd4
lumahloi <115045300+lumahloi@users.noreply.github.com>
2024-09-09 14:53:20 -0300

    feat: cadastro regex

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -18,34 +18,46 @@ module.exports = {
 
     async create(request, response) {
         try {
-            //checar se email ja existe
             const { name, email, password, picture } = request.body;
-            const id = crypto.randomBytes(4).toString('HEX');
-    
-            // Criptografar email e senha em paralelo
-            const salt = await bcrypt.genSalt(10);
-            const [hashedPassword] = await Promise.all([
-                bcrypt.hash(password, salt),
-            ]);
 
-            // Fazer validação de itens...
-    
-            // Adicionar timeout para a operação de inserção
-            const insertPromise = connection('users').insert({
-                id,
-                name,
-                email,
-                password: hashedPassword,
-                picture
-            });
-    
-            const timeoutPromise = new Promise((_, reject) =>
-                setTimeout(() => reject(new Error('Timeout reached')), 10000) // Timeout de 10 segundos
-            );
-    
-            await Promise.race([insertPromise, timeoutPromise]);
-    
-            return response.status(200).json({ message: 'Usuário criado com sucesso' });
+            if(name && email && password){
+                const nameCheck = name.length
+                const emailCheck = email.length
+                const passwordCheck = password.length
+
+                if((nameCheck >= 5 && nameCheck <= 18)&&(emailCheck <= 30)&&(passwordCheck >= 8 && passwordCheck <= 20)){
+                    const regexName = /^[a-zA-Z0-9]*$/
+                    const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+                    if(!regexName.test(name) || !regexEmail.test(emailCheck)){
+                        const id = crypto.randomBytes(4).toString('HEX');
+                
+                        // Criptografar email e senha em paralelo
+                        const salt = await bcrypt.genSalt(10);
+                        const [hashedPassword] = await Promise.all([
+                            bcrypt.hash(password, salt),
+                        ]);
+                
+                        // Adicionar timeout para a operação de inserção
+                        const insertPromise = connection('users').insert({
+                            id,
+                            name,
+                            email,
+                            password: hashedPassword,
+                            picture
+                        });
+                
+                        const timeoutPromise = new Promise((_, reject) =>
+                            setTimeout(() => reject(new Error('Timeout reached')), 10000) // Timeout de 10 segundos
+                        );
+                
+                        await Promise.race([insertPromise, timeoutPromise]);
+                
+                        return response.status(200).json({ message: 'Usuário criado com sucesso' });
+                    }
+                }
+            }
+            return response.status(400).json({ error: 'Operação não permitida.' });
         } catch (error) {
             console.error(error);
             return response.status(500).json({ error: 'Erro interno do servidor.' });
