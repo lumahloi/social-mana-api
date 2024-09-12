73479c1ddc4032928e866c4ca23031b207fdb57e
lumahloi <115045300+lumahloi@users.noreply.github.com>
2024-09-12 14:02:03 -0300

    fix:railway

--- src/controllers/BdController.js
+++ src/controllers/BdController.js
@@ -6,63 +6,24 @@ export const BdController = {
         try {
             connection = await createConnection();
 
-            // Inserir novo usuário
+            // Remover chave primária existente
+            await connection.query('ALTER TABLE users DROP PRIMARY KEY');
+            await connection.query('ALTER TABLE posts DROP PRIMARY KEY');
+            await connection.query('ALTER TABLE likes DROP PRIMARY KEY');
+            await connection.query('ALTER TABLE dislikes DROP PRIMARY KEY');
+
+            // Atualizar colunas e adicionar chaves primárias
             await connection.query(
-                'ALTER TABLE users MODIFY COLUMN id VARCHAR(9) PRIMARY KEY'
+                'ALTER TABLE users MODIFY COLUMN id VARCHAR(9), MODIFY COLUMN name VARCHAR(45) NOT NULL, MODIFY COLUMN email VARCHAR(45) NOT NULL, MODIFY COLUMN password VARCHAR(60) NOT NULL, MODIFY COLUMN picture VARCHAR(10), ADD PRIMARY KEY (id)'
             );
             await connection.query(
-                'ALTER TABLE users MODIFY COLUMN name VARCHAR(45) NOT NULL'
+                'ALTER TABLE posts MODIFY COLUMN id INT AUTO_INCREMENT, MODIFY COLUMN description VARCHAR(300) NOT NULL, MODIFY COLUMN userid VARCHAR(10) NOT NULL, ADD PRIMARY KEY (id), ADD CONSTRAINT fk_posts_users FOREIGN KEY(userid) REFERENCES users(id)'
             );
             await connection.query(
-                'ALTER TABLE users MODIFY COLUMN email VARCHAR(45) NOT NULL'
+                'ALTER TABLE likes MODIFY COLUMN id INT AUTO_INCREMENT, MODIFY COLUMN userid VARCHAR(10) NOT NULL, MODIFY COLUMN postid INT NOT NULL, ADD PRIMARY KEY (id), ADD CONSTRAINT fk_likes_users FOREIGN KEY(userid) REFERENCES users(id), ADD CONSTRAINT fk_likes_posts FOREIGN KEY(postid) REFERENCES posts(id)'
             );
             await connection.query(
-                'ALTER TABLE users MODIFY COLUMN password VARCHAR(60) NOT NULL'
-            );
-            await connection.query(
-                'ALTER TABLE users MODIFY COLUMN picture VARCHAR(10)'
-            );
-            await connection.query(
-                'ALTER TABLE posts MODIFY COLUMN id INT AUTO_INCREMENT PRIMARY KEY'
-            );
-            await connection.query(
-                'ALTER TABLE posts MODIFY COLUMN description VARCHAR(300) NOT NULL'
-            );
-            await connection.query(
-                'ALTER TABLE posts MODIFY COLUMN userid VARCHAR(10) NOT NULL'
-            );
-            await connection.query(
-                'ALTER TABLE posts ADD CONSTRAINT fk_posts_users FOREIGN KEY(userid) REFERENCES users(id)'
-            );
-            await connection.query(
-                'ALTER TABLE likes MODIFY COLUMN id INT AUTO_INCREMENT PRIMARY KEY'
-            );
-            await connection.query(
-                'ALTER TABLE likes MODIFY COLUMN userid VARCHAR(10) NOT NULL'
-            );
-            await connection.query(
-                'ALTER TABLE likes MODIFY COLUMN postid INT NOT NULL'
-            );
-            await connection.query(
-                'ALTER TABLE likes ADD CONSTRAINT fk_likes_users FOREIGN KEY(userid) REFERENCES users(id)'
-            );
-            await connection.query(
-                'ALTER TABLE likes ADD CONSTRAINT fk_likes_posts FOREIGN KEY(postid) REFERENCES posts(id)'
-            );
-            await connection.query(
-                'ALTER TABLE dislikes MODIFY COLUMN id INT AUTO_INCREMENT PRIMARY KEY'
-            );
-            await connection.query(
-                'ALTER TABLE dislikes MODIFY COLUMN userid VARCHAR(10) NOT NULL'
-            );
-            await connection.query(
-                'ALTER TABLE dislikes MODIFY COLUMN postid INT NOT NULL'
-            );
-            await connection.query(
-                'ALTER TABLE dislikes ADD CONSTRAINT fk_dislikes_users FOREIGN KEY(userid) REFERENCES users(id)'
-            );
-            await connection.query(
-                'ALTER TABLE dislikes ADD CONSTRAINT fk_dislikes_posts FOREIGN KEY(postid) REFERENCES posts(id)'
+                'ALTER TABLE dislikes MODIFY COLUMN id INT AUTO_INCREMENT, MODIFY COLUMN userid VARCHAR(10) NOT NULL, MODIFY COLUMN postid INT NOT NULL, ADD PRIMARY KEY (id), ADD CONSTRAINT fk_dislikes_users FOREIGN KEY(userid) REFERENCES users(id), ADD CONSTRAINT fk_dislikes_posts FOREIGN KEY(postid) REFERENCES posts(id)'
             );
 
             return response.status(200).json({ message: 'Alterações no banco de dados concluídas com sucesso.' });
