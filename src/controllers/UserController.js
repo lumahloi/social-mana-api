ad6f70f70fa5a21195a8815244b2fda09aaf88e1
lumahloi <115045300+lumahloi@users.noreply.github.com>
2024-09-12 12:58:52 -0300

    fix:railway

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -8,94 +8,69 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const UserController = {
-    // async create(request, response) {
-    //     let connection;
-    //     try {
-    //         const { name, email, password, picture } = request.body;
-
-    //         if (name && email && password) {
-    //             const nameCheck = name.length;
-    //             const emailCheck = email.length;
-    //             const passwordCheck = password.length;
-
-    //             if ((nameCheck >= 5 && nameCheck <= 18) && (emailCheck <= 30) && (passwordCheck >= 8 && passwordCheck <= 20)) {
-    //                 const regexName = /^[a-zA-Z0-9]+$/;
-    //                 const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    //                 const regexSpace = /\s/;
-
-    //                 if (regexName.test(name) && regexEmail.test(email) && !regexSpace.test(name)) {
-    //                     connection = await createConnection();
-
-    //                     // Verificar se o email já está cadastrado
-    //                     const existingEmail = await check('users', 'email', email);
-    //                     if (existingEmail.length === 0) {
-    //                         const id = crypto.randomBytes(4).toString('HEX');
-
-    //                         // Criptografar senha
-    //                         const salt = await bcrypt.genSalt(10);
-    //                         const hashedPassword = await bcrypt.hash(password, salt);
-
-    //                         // Inserir novo usuário
-    //                         const insertPromise = new Promise((resolve, reject) => {
-    //                             connection.query('INSERT INTO users (id, name, email, password, picture) VALUES (?, ?, ?, ?, ?)', [id, name, email, hashedPassword, picture], (err) => {
-    //                                 if (err) {
-    //                                     return reject(err);
-    //                                 }
-    //                                 resolve();
-    //                             });
-    //                         });
-
-    //                         const timeoutPromise = new Promise((_, reject) =>
-    //                             setTimeout(() => reject(new Error('Timeout reached')), 10000) // Timeout de 10 segundos
-    //                         );
-
-    //                         await Promise.race([insertPromise, timeoutPromise]);
-
-    //                         return response.status(200).json({ message: 'Usuário criado com sucesso' });
-    //                     } else {
-    //                         return response.status(400).json({ message: 'Email já cadastrado.' });
-    //                     }
-    //                 } else {
-    //                     return response.status(400).json({ message: 'Insira nome sem caracteres especiais e email válido.' });
-    //                 }
-    //             } else {
-    //                 return response.status(400).json({ message: 'Insira informações dentro do limite de caracteres.' });
-    //             }
-    //         } else {
-    //             return response.status(400).json({ message: 'Insira nome, email e senha.' });
-    //         }
-    //     } catch (error) {
-    //         console.error(error);
-    //         return response.status(500).json({ error: 'Erro no servidor, tente novamente mais tarde.' });
-    //     } finally {
-    //         if (connection) {
-    //             await connection.end();
-    //         }
-    //     }
-    // },
-
     async create(request, response) {
-        let connection
-
+        let connection;
         try {
-            connection = await createConnection();
-            const insertPromise = new Promise((resolve, reject) => {
-                connection.query('DROP TABLE IF EXISTS users', (err) => { return err });
-                connection.query('DROP TABLE IF EXISTS posts', (err) => { /* handle error */ return err});
-                connection.query('DROP TABLE IF EXISTS likes', (err) => { /* handle error */ return err});
-                connection.query('DROP TABLE IF EXISTS dislikes', (err) => { /* handle error */ return err});
+            const { name, email, password, picture } = request.body;
 
-            });
+            if (name && email && password) {
+                const nameCheck = name.length;
+                const emailCheck = email.length;
+                const passwordCheck = password.length;
 
-            const timeoutPromise = new Promise((_, reject) =>
-                setTimeout(() => reject(new Error('Timeout reached')), 10000) // Timeout de 10 segundos
-            );
+                if ((nameCheck >= 5 && nameCheck <= 18) && (emailCheck <= 30) && (passwordCheck >= 8 && passwordCheck <= 20)) {
+                    const regexName = /^[a-zA-Z0-9]+$/;
+                    const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+                    const regexSpace = /\s/;
 
-            await Promise.race([insertPromise, timeoutPromise]);
+                    if (regexName.test(name) && regexEmail.test(email) && !regexSpace.test(name)) {
+                        connection = await createConnection();
 
-            return response.status(200).json({ message: 'Usuário criado com sucesso' });
-        } catch (e) {
-            return response.status(400).json({ message: 'Erro' });
+                        // Verificar se o email já está cadastrado
+                        const existingEmail = await check('users', 'email', email);
+                        if (existingEmail.length === 0) {
+                            const id = crypto.randomBytes(4).toString('HEX');
+
+                            // Criptografar senha
+                            const salt = await bcrypt.genSalt(10);
+                            const hashedPassword = await bcrypt.hash(password, salt);
+
+                            // Inserir novo usuário
+                            const insertPromise = new Promise((resolve, reject) => {
+                                connection.query('INSERT INTO users (id, name, email, password, picture) VALUES (?, ?, ?, ?, ?)', [id, name, email, hashedPassword, picture], (err) => {
+                                    if (err) {
+                                        return reject(err);
+                                    }
+                                    resolve();
+                                });
+                            });
+
+                            const timeoutPromise = new Promise((_, reject) =>
+                                setTimeout(() => reject(new Error('Timeout reached')), 10000) // Timeout de 10 segundos
+                            );
+
+                            await Promise.race([insertPromise, timeoutPromise]);
+
+                            return response.status(200).json({ message: 'Usuário criado com sucesso' });
+                        } else {
+                            return response.status(400).json({ message: 'Email já cadastrado.' });
+                        }
+                    } else {
+                        return response.status(400).json({ message: 'Insira nome sem caracteres especiais e email válido.' });
+                    }
+                } else {
+                    return response.status(400).json({ message: 'Insira informações dentro do limite de caracteres.' });
+                }
+            } else {
+                return response.status(400).json({ message: 'Insira nome, email e senha.' });
+            }
+        } catch (error) {
+            console.error(error);
+            return response.status(500).json({ error: 'Erro no servidor, tente novamente mais tarde.' });
+        } finally {
+            if (connection) {
+                await connection.end();
+            }
         }
-    }
+    },
 };
